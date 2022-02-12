a4b2fc5264c52153f119bb907241b78164b8e523
piupiunn <96390657+piupiunn@users.noreply.github.com>
2022-02-12 06:07:58 +0300

    fetchs

--- src/EventList.js
+++ src/EventList.js
@@ -4,7 +4,7 @@ export default function EventList({ events, handleEvent }) {
   return (
     <div>
       {events.map((event) => (
-        <div key={event.key}>
+        <div>
           <h2>{event.title}</h2>
           <p>
             {event.location} - {event.date}
